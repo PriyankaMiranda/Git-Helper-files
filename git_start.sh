761ddfed98254fa38b360c41c119203ca26861b8
Priyanka Miranda <priyankamiranda2@gmail.com>
2020-04-12 19:27:22 +0530

    update git_start

--- git_start.sh
+++ git_start.sh
@@ -9,80 +9,105 @@
 
 # --------------------------USAGE-----------------------------------------
 # run git_start.sh on command line with the following arguments
-# -r - REPOSITORY NAME 
-# -d - DESCRIPTION
-# -p - PROJECT PATH
+# -p - PROJECT PATH (mandatory)
+# -r - REPOSITORY NAME (not mandatory)
+# -d - YOU WANT TO WRITE DESCRIPTION (not mandatory)
 
 ################################################################################
 
-while getopts r:d:p: Options;
+while getopts p:r:d: Options;
 	do
 	case "$Options" in	
-
-	# Option s - Specify Source Folder Location
-	r)	# Read Source Folder Location
-		sDir=$OPTARG
-		echo "Option -r used. Source Folder: $OPTARG"
-		;;
-
-	# Option d - Specify Destination Folder Location
-	d)	# Read Destination Folder Location
-		dDir=$OPTARG
-		echo "Option -d used. Destination Folder: $OPTARG"
-		;;
 		
-	# Option n - Number of Characters to Retain
-	p)	# Read Number of Characters to Retain
-		nChar=$OPTARG
-		echo "Option -p used. Retaining $nChar Characters."
+	# Option n - Specify Project Path
+	p)	# Read Project Path
+		PROJECT_PATH=$OPTARG
+		echo "Local Absolute Project Path: $OPTARG"
 		;;
-		
+
+	# Option s - Specify Repository Name
+	r)	# Read Repository Name
+		REPO_NAME=$OPTARG
+		echo "Repository Name: $OPTARG"
+		;;
+
+	# Option d - Specify Description
+	d)	# Read Description Option
+		DESCRIP=$OPTARG
+		SELECTED='y'
+		if [ "$DESCRIP" == "$SELECTED" ]; then
+		    echo "You have chosen to write a description"
+		    echo "Please write your description:"
+		    read DESCRIPTION
+		else
+		    echo "You have chosen not to write a description"
+		    DESCRIPTION=""
+		fi
+		;;
+
 	# Unspecified Option
 	\?)	# Issue an Error Message
 		echo "Invalid Option!"
 		echo "---------------------------CORRECT USAGE---------------------------"
-		echo "bash git_start.sh -r <REPOSITORY NAME> -d <DESCRIPTION> -p <PROJECT PATH>" 
+		echo "bash git_start.sh -r <REPOSITORY NAME> -d <y for yes/anything else for no> -p <LOCAL ABSOLUTE PROJECT PATH>" 
 		;;		
+
     esac
 done
 
-source .gitignore/authentication.sh
+source authentication.sh
+
+# When project path isn't specified
+if [ -z "$PROJECT_PATH" ]; then
+    echo "You have not provided a project path!"
+    exit 1
+fi		
+
+# When repository isn't specified
+if [ -z "$REPO_NAME" ]; then
+    echo "Repository name is going to be set as your project folder."
+    REPO_NAME=${PROJECT_PATH##*/} 
+    echo "Repository Name: $REPO_NAME"
+fi		
 
 
-# CURRENTDIR=${pwd}
 
-# EMAIL_ID=${login_email}
-# GITHUB_PWD=${github_password}
-# GITLAB_PWD=${gitlab_password}
+CURRENTDIR=${pwd}
 
-# # step 1: name of the remote repo. Enter a SINGLE WORD ..or...separate with hyphens
-# echo "What name do you want to give your remote repo?"
-# read REPO_NAME
+USERNAME=${login_email}
+GITHUB_PWD=${github_password}
+GITLAB_PWD=${gitlab_password}
 
-# echo "Enter a repo description: "
-# read DESCRIPTION
+echo "Your login-id : $USERNAME"
+echo "Your github password : $GITHUB_PWD"
+echo "Your gitlab password : $GITLAB_PWD"
+echo "REPO NAME : $REPO_NAME"
 
-
-# # step 2:  the local project folder path
-# echo "what is the absolute path to your local project directory?"
-# read PROJECT_PATH
-
-# echo "What is your github username?"
-# read USERNAME
-
-# # step 3 : go to path 
-# cd "$PROJECT_PATH"
+# -------------------------------------------------------------
+echo "Press Ctrl+C if anything specified above is wrong."
+echo "Go through README for instructions on correct usage."
+echo "-----------------------------------------------------"
+echo "If everything is correct, press Enter."
+read cache
+unset cache
+echo "Continued!!!"
 
 
-# # step 4: initialise the repo locally, create blank README, add and commit
-# git init
-# touch README.MD
-# git add README.MD
-# git commit -m 'initial commit -setup with .sh script'
+
+
+# Go to path 
+cd "$PROJECT_PATH"
+
+
+# Initialise the repo locally, create blank README, add and commit
+git init
+touch README.MD
+git add README.MD
+git commit -m 'Initial Commit with .sh script'
 
 
 # # step 5 use github API to log the user in
-# curl -u ${USERNAME} https://api.github.com/user/repos -d "{\"name\": \"${REPO_NAME}\", \"description\": \"${DESCRIPTION}\"}"
+curl -u ${USERNAME} https://api.github.com/user/repos -d "{\"name\": \"${REPO_NAME}\", \"description\": \"${DESCRIPTION}\"}"
 
 # #  step 6 add the remote github repo to local repo and push
 # git remote add origin https://github.com/${USERNAME}/${REPO_NAME}.git
